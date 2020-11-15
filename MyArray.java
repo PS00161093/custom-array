7f24d8881d025c8018d569fddd145a3153333631
ps29653 <SharmaPrashant@JohnDeere.com>
2020-11-15 15:35:55 +0530

    Added push() & pop()

--- MyArray.java
+++ MyArray.java
@@ -1,10 +1,21 @@
+import java.util.Arrays;
+
 class TestClient {
 
     public static void main(String[] args) {
 
-        int[] array = {2, 6, 9};
-        MyArray myArray = new MyArray(3, array);
+        MyArray myArray = new MyArray(3);
+
         System.out.println(myArray.toString());
+
+        myArray.push(1); myArray.push(7); myArray.push(9);
+
+        System.out.println(myArray.toString());
+
+        myArray.pop();
+
+        System.out.println(myArray.toString());
+
     }
 }
 
@@ -14,16 +25,31 @@ class MyArray {
 
     private int[] array;
 
-    MyArray(int length, int[] array) {
+    MyArray(int length) {
 
         this.length = length;
-        this.array = array;
+        this.array = new int[length];
     }
 
     public String toString() {
 
-        return "Array is of length: " + this.length;
+        return Arrays.toString(this.array);
     }
 
+    public void push(int element){
+
+        for(int i = 0; i < this.length; i++){
+
+            if(this.array[i] == 0 && element != 0){
+                this.array[i] = element;
+                break;
+            }
+        }
+    }
+
+    public void pop(){
+
+        this.array[this.length - 1] = 0;
+    }
 
 }

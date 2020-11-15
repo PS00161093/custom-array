fd92fd0a96cd88a1de593d8308de1b53e9c08446
ps29653 <SharmaPrashant@JohnDeere.com>
2020-11-15 21:31:28 +0530

    check if duplicate elements

--- src/DuplicateItem.java
+++ src/DuplicateItem.java
@@ -7,7 +7,7 @@ public class DuplicateItem {
 
     public static void main(String[] args) {
 
-        int[] input = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+        int[] input = {1, 1, 1, 3, 3, 4, 3, 2, 4};
         System.out.println(haveDuplicateElements(input));
     }
 

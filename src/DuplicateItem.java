b7dbdf18d928965ccbcfded6cf53bd0c32bab241
ps29653 <SharmaPrashant@JohnDeere.com>
2020-11-15 21:31:13 +0530

    check if duplicate elements

---
+++ src/DuplicateItem.java
@@ -0,0 +1,25 @@
+package src;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class DuplicateItem {
+
+    public static void main(String[] args) {
+
+        int[] input = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+        System.out.println(haveDuplicateElements(input));
+    }
+
+    private static boolean haveDuplicateElements(int[] input) {
+        Set<Integer> set = new HashSet<>();
+        for (int i : input) {
+            if (set.contains(i)) {
+                return true;
+            } else {
+                set.add(i);
+            }
+        }
+        return false;
+    }
+}

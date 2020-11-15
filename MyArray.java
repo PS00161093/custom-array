64a39f10fede49f4dcf1777e9c53dfac1537093b
ps29653 <SharmaPrashant@JohnDeere.com>
2020-11-15 15:12:11 +0530

    Added toString()

--- MyArray.java
+++ MyArray.java
@@ -1,23 +1,29 @@
-class MyArray {
-
-    private length ;
-
-    private int[]  array;
-
-    public MyArray(int length, int[] array){
-
-        this.length = length;
-        this.array = array
-
-    }
-
-}
-
-class TestClient{
+class TestClient {
 
     public static void main(String[] args) {
 
-        MyArray myArray = new MyArray(3, {7,1,0});
-
+        int[] array = {2, 6, 9};
+        MyArray myArray = new MyArray(3, array);
+        System.out.println(myArray.toString());
     }
+}
+
+class MyArray {
+
+    private int length;
+
+    private int[] array;
+
+    MyArray(int length, int[] array) {
+
+        this.length = length;
+        this.array = array;
+    }
+
+    public String toString() {
+
+        return "Array is of length: " + this.length;
+    }
+
+
 }

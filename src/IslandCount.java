8b7bf7ea4a8d4e3b610e82cf13b703b8c4465314
Prashant Sharma <prashant.sharma1thoughtworks.com>
2021-10-02 20:41:11 +0530

    find-the-number-of-islands

--- src/IslandCount.java
+++ src/IslandCount.java
@@ -23,9 +23,7 @@ public class IslandCount {
         if (grid == null || grid.length == 0) return res;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
-                if (grid[i][j] == '1') {
-                    res += dfs(grid, i, j);
-                }
+                if (grid[i][j] == '1') res += dfs(grid, i, j);
             }
         }
 
@@ -40,13 +38,10 @@ public class IslandCount {
 
         dfs(grid, i, j + 1);
         dfs(grid, i, j - 1);
-
         dfs(grid, i + 1, j);
         dfs(grid, i - 1, j);
-
         dfs(grid, i - 1, j + 1);
         dfs(grid, i + 1, j - 1);
-
         dfs(grid, i - 1, j - 1);
         dfs(grid, i + 1, j + 1);
 

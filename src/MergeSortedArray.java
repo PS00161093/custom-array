90c2e8b9e35a59f796dbfca00b90dc41ab47616f
Prashant Sharma <prashant.sharma1thoughtworks.com>
2021-10-25 15:53:57 +0530

    merge-sorted-array | O(n)

--- src/MergeSortedArray.java
+++ src/MergeSortedArray.java
@@ -13,7 +13,32 @@ public class MergeSortedArray {
         int[] firstArray = {3, 6, 9, 11, 13, 0, 0, 0, 0};
         int[] secondArray = {1, 3, 9, 10};
         mergeAndSort(firstArray, 5, secondArray, secondArray.length);
-        stream(firstArray).forEach(System.out::print);
+        stream(firstArray).mapToObj(i -> i + ", ").forEach(System.out::print);
+    }
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int tail1 = m - 1;
+        int tail2 = n - 1;
+        int finished = m + n - 1;
+
+        while (tail1 >= 0 && tail2 >= 0) {
+            if ((nums1[tail1] > nums2[tail2])) {
+                nums1[finished] = nums1[tail1];
+                tail1--;
+            } else {
+                nums1[finished] = nums2[tail2--];
+            }
+
+            finished--;
+        }
+
+        while (tail2 >= 0) {
+            nums1[finished] = nums2[tail2];
+            tail2--;
+            finished--;
+        }
+
     }
 
     public static void mergeAndSort(int[] firstArray, int m, int[] secondArray, int n) {
